662cd1cf0dc8aecf451aace8df1318a234802c6c
Md. Ahasan <mdahasanulhoque15@gmail.com>
2024-01-14 02:12:43 +0600

    update

--- app/src/main/java/com/kingtech/funbook/fragments/HomeFragment.kt
+++ app/src/main/java/com/kingtech/funbook/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.kingtech.funbook.R
 import com.kingtech.funbook.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -15,6 +16,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
         binding.vpHomeDetail.adapter = adapter
 
        binding.tlHomeDetailBar.setupWithViewPager(binding.vpHomeDetail)
+        binding.apply {
+            tlHomeDetailBar.getTabAt(0)!!.setIcon(R.drawable.round_home_24)
+            tlHomeDetailBar.getTabAt(1)!!.setIcon(R.drawable.baseline_diversity_1_24)
+            tlHomeDetailBar.getTabAt(2)!!.setIcon(R.drawable.round_desktop_mac_24)
+            tlHomeDetailBar.getTabAt(3)!!.setIcon(R.drawable.baseline_emoji_emotions_24)
+            tlHomeDetailBar.getTabAt(4)!!.setIcon(R.drawable.funbook_logo)
+        }
     }
 
 
@@ -22,7 +30,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
 
     class HomePagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
-        private val tabTitles = arrayOf("Home", "Friends","Memes","Profile")
+        private val fragmentlist = ArrayList<Fragment>()
+        private val tabTitles = arrayOf("", "","","","")
 
         override fun getItem(position: Int): Fragment {
             return when (position) {
@@ -30,6 +39,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
                 1 -> UserFragment()
                 2 -> MemesFragment()
                 3 -> PostFragment()
+                4 -> PostFragment()
                 else -> PostFragment()
 //                throw IllegalArgumentException("Invalid position: $position")
             }
