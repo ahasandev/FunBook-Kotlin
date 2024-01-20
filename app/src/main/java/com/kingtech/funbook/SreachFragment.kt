b5753674b8d989ce72ca7f96187915c0ebddf275
Md. Ahasan <mdahasanulhoque15@gmail.com>
2024-01-20 23:36:30 +0600

    update

---
+++ app/src/main/java/com/kingtech/funbook/SreachFragment.kt
@@ -0,0 +1,14 @@
+package com.kingtech.funbook
+
+import android.os.Bundle
+import android.view.View
+import com.kingtech.funbook.databinding.FragmentSreachBinding
+import com.kingtech.funbook.fragments.BaseFragment
+
+class SreachFragment : BaseFragment<FragmentSreachBinding>(FragmentSreachBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/kingtech/funbook/fragments/HomeFragment.kt
+++ app/src/main/java/com/kingtech/funbook/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.kingtech.funbook.R
+import com.kingtech.funbook.SreachFragment
 import com.kingtech.funbook.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -21,8 +22,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
             tlHomeDetailBar.getTabAt(0)!!.setIcon(R.drawable.round_home_24)
             tlHomeDetailBar.getTabAt(1)!!.setIcon(R.drawable.baseline_diversity_1_24)
             tlHomeDetailBar.getTabAt(2)!!.setIcon(R.drawable.round_desktop_mac_24)
-            tlHomeDetailBar.getTabAt(3)!!.setIcon(R.drawable.baseline_emoji_emotions_24)
-            tlHomeDetailBar.getTabAt(4)!!.setIcon(R.drawable.round_account_circle_24)
+            tlHomeDetailBar.getTabAt(3)!!.setIcon(R.drawable.rounded_content_paste_search_24)
+            tlHomeDetailBar.getTabAt(4)!!.setIcon(R.drawable.baseline_emoji_emotions_24)
+            tlHomeDetailBar.getTabAt(5)!!.setIcon(R.drawable.round_account_circle_24)
         }
     }
 
@@ -32,14 +34,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     class HomePagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
         private val fragmentlist = ArrayList<Fragment>()
-        private val tabTitles = arrayOf("", "","","","")
+        private val tabTitles = arrayOf("", "","","","","")
 
         override fun getItem(position: Int): Fragment {
             return when (position) {
                 0 -> PostFragment()
                 1 -> UserFragment()
                 2 -> MemesFragment()
-                3 -> PostFragment()
+                3 -> SreachFragment()
                 4 -> PostFragment()
                 else -> PostFragment()
 //                throw IllegalArgumentException("Invalid position: $position")
